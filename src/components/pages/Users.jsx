6aa9267e799ef61e15a5a366a63fc9f3150615b1
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-10-24 17:47:21 -0300

    Adiciona tela de usuário - Loading

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -3,19 +3,24 @@ import React from "react";
 
 import Default from "../templates/Default";
 import UserListWrapper from "../molecules/UserListWrapper";
+import AppLoading from "../organisms/AppLoading";
 
 export default function Users() {
   const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://63cf09718a780ae6e6710dbe.mockapi.io/users")
       .then((response) => response.json())
       .then((data) => {
         setUsers(data);
+        setIsLoading(false);
       });
   }, []);
 
-  return (
+  return isLoading ? (
+    <AppLoading />
+  ) : (
     <Default>
       <div className="users">
         <h1>Users</h1>

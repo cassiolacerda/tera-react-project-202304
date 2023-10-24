b258a3bf6e894be97faa37c1b70717bfa6455cd0
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-10-24 18:18:43 -0300

    Adiciona tela de usuário - Bônus

--- src/components/molecules/UserListItem.jsx
+++ src/components/molecules/UserListItem.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { GlobalContext } from "../../contexts/GlobalContext";
+
 export default function UserListItem(props) {
+  const { currentUser } = React.useContext(GlobalContext);
+
+  const classNames = [
+    "users__list-item",
+    ...(currentUser === props.user.id.toString() ? ["active"] : []),
+  ].join(" ");
+
   return (
-    <Link to={`/users/${props.user.id}`} className="users__list-item">
+    <Link to={`/users/${props.user.id}`} className={classNames}>
       <div className="users__list-item-photo">
         <img src={props.user.avatar} className="responsive avatar" alt="" />
       </div>
